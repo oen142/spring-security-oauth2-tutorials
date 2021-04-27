2005a2dccd5ccd3cdb3edde859d02e19f04d3c33
oen142 <oen142@naver.com>
2021-04-27 22:13:59 +0900

    Initial commit

---
+++ src/main/java/com/wani/springsecurityoauth2tutorial/SpringSecurityOauth2TutorialApplication.java
@@ -0,0 +1,13 @@
+package com.wani.springsecurityoauth2tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2TutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2TutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wani/springsecurityoauth2tutorial/SpringSecurityOauth2TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.springsecurityoauth2tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2TutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
